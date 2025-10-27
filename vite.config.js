454418d70e773fa14dc0a49e0e4b832eaa91626c
sourjyendu-barik <sourjyendubarik7798@gmail.com>
2025-10-27 19:30:11 +0530

    corrected vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // 👈 THIS LINE FIXES THE REFRESH 404 ISSUE
+
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+});
