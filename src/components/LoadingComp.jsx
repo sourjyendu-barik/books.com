129c3ab89ec499e0c63584cc6705074a1c0562a9
sourjyendu-barik <sourjyendubarik7798@gmail.com>
2026-07-22 19:08:15 +0530

    added loader

--- src/components/LoadingComp.jsx
+++ src/components/LoadingComp.jsx
@@ -1,5 +1,19 @@
 const LoadingComp = ({ message = "Loading..." }) => {
-  return <p>{message}</p>;
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="text-center">
+        <div
+          className="spinner-border text-primary"
+          style={{ width: "3rem", height: "3rem" }}
+          role="status"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </div>
+
+        <p className="mt-3 fs-5">{message}</p>
+      </div>
+    </div>
+  );
 };
 
 export default LoadingComp;
