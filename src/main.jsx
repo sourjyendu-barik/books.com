321a0b582a30a25ed266bd0be0fdba9061faec03
sourjyendu-barik <sourjyendubarik7798@gmail.com>
2026-07-22 20:22:28 +0530

    removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId="907261524450-nnaf9fa707m1o80v2dt17kuficsod00f.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="907261524450-nnaf9fa707m1o80v2dt17kuficsod00f.apps.googleusercontent.com">
+    <App />
+  </GoogleOAuthProvider>,
 );
